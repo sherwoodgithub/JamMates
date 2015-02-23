5afb76f3946d81867629b765761f90a347a87117
Stephen Sherwood <stephenemails@gmail.com>
2015-02-23 12:54:14 -0800

    Initial Commit

---
+++ JamMates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JamMates
+//
+//  Created by Stephen Sherwood on 2/23/15.
+//  Copyright (c) 2015 Stephen Sherwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JamMates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JamMates
+//
+//  Created by Stephen Sherwood on 2/23/15.
+//  Copyright (c) 2015 Stephen Sherwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
