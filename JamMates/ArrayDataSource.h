ab62b7bd09e07af4c382ab24e23a32dc71d5d576
Stephen Sherwood <stephenemails@gmail.com>
2015-02-23 13:00:57 -0800

    Initial Local

---
+++ JamMates/ArrayDataSource.h
@@ -0,0 +1,24 @@
+//
+//  ArrayDataSource.h
+//  JamMates
+//
+//  Created by Stephen Sherwood on 2/23/15.
+//  Copyright (c) 2015 Stephen Sherwood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "UIKit/UIKit.h"
+
+
+typedef void (^TableViewCellConfigureBlock)(id cell, id item);
+
+
+@interface ArrayDataSource : NSObject <UITableViewDataSource>
+
+- (id)initWithItems:(NSArray *)anItems
+     cellIdentifier:(NSString *)aCellIdentifier
+ configureCellBlock:(TableViewCellConfigureBlock)aConfigureCellBlock;
+
+- (id)itemAtIndexPath:(NSIndexPath *)indexPath;
+
+@end
